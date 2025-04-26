d243960bb6035f167a974d7e3d837dcae3bc3024
rasa9521 <rsrasasingh@gmail.com>
2025-04-26 18:48:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = "7472777466:AAHdCsYtZpPnoMv67OL5_Hv2yjvy2Nz8jEg"
-    API_ID = 22581733
-    API_HASH = "1db7bdcf908100cc641c6a5276765c3d"
+    BOT_TOKEN = "8003649544:AAGoiThVN8KLJyKGsGf1BcfTsjDTrSmjFR8"
+    API_ID = 27900743
+    API_HASH = "ebb06ea8d41420e60b29140dcee902fc"
     ADMIN = '6530997270'.split(',')
     ADMIN_ID = [int(id) for id in ADMIN]
-    DB_URL = "mongodb+srv://neoayush444:3kuwGf9jKicbDxvT@cluster0.f9vq7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL = "mongodb+srv://rsrasasingh:FS2G9YbI28KbPHLC@cluster0.yljr3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DB_NAME = "Crack_Jack"
-    TXT_LOG = -1002294532244
-    AUTH_LOG = -1002294532244
-    HIT_LOG = -1002294532244
-    DRM_DUMP = -1002294532244
-    CHANNEL = --1002487777733
+    TXT_LOG = -1002566364060
+    AUTH_LOG = -1002566364060
+    HIT_LOG = -1002566364060
+    DRM_DUMP = -1002566364060
+    CHANNEL = -1002566364060
     CH_URL = "https://t.me/justfortestinggwak"
     OWNER = "https://t.me/mrcraacker"
     THUMB_URL = "https://vault.pictures/media/images/b9/ad/12/b9ad123d95cd49465fcb954d.jpg" #Replace by with your Thumb URL
